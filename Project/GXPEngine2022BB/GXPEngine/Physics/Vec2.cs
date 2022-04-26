d00894387e4a6eed9f02d0a4470820df48a3127f
Joris327 <joris.van.bellen@gmail.com>
2022-04-26 10:17:50 +0200

    expanded Vec2

--- Project/GXPEngine2022BB/GXPEngine/Physics/Vec2.cs
+++ Project/GXPEngine2022BB/GXPEngine/Physics/Vec2.cs
@@ -12,68 +12,158 @@ public struct Vec2
 		y = pY;
 	}
 
+	public float Length()
+	{
+		float length;
+		length = Mathf.Sqrt((x * x) + (y * y));
+		return length;
+	}
+
 	public void Normalize()
 	{
-		float len = this.Length();
-		if (len > 0)
-		{
-			this.x /= len;
-			this.y /= len;
-		}
+		if (Length() == 0) return;
 
+		float scalar = 1 / Length();
+		x *= scalar;
+		y *= scalar;
 	}
 
 	public Vec2 Normalized()
 	{
-		float len = this.Length();
-		if (len > 0)
-		{
-			float newX = this.x / len;
-			float newY = this.y / len;
-			return new Vec2(newX, newY);
-		}
-		else
-		{
-			return new Vec2(0, 0);
-		}
+		if (Length() == 0) return this;
 
-
+		Vec2 normalize;
+		float scalar = 1 / Length();
+		normalize = new Vec2(x * scalar, y * scalar);
+		return normalize;
 	}
 
-	public float Length()
+	public void SetXY(float setX, float setY)
 	{
-		float pyth = (this.x * this.x) + (this.y * this.y);
-		float len = Mathf.Sqrt(pyth);
-		return len;
+		x = setX;
+		y = setY;
 	}
 
-	public void SetXY(float newX, float newY)
+	public static Vec2 operator +(Vec2 left, Vec2 right)    //add
 	{
-		this.x = newX;
-		this.y = newY;
+		return new Vec2(left.x + right.x, left.y + right.y);
 	}
-
-	public static Vec2 operator *(Vec2 v, float scale)
+	public static Vec2 operator +(float left, Vec2 right)
 	{
-		float newX = v.x *= scale;
-		float newY = v.y *= scale;
-
-		return new Vec2(newX, newY);
+		return new Vec2(left + right.x, left + right.y);
+	}
+	public static Vec2 operator +(Vec2 left, float right)
+	{
+		return new Vec2(left.x + right, left.y + right);
 	}
 
-	public static Vec2 operator -(Vec2 left, Vec2 right)
+	public static Vec2 operator -(Vec2 left, Vec2 right) //substract
 	{
 		return new Vec2(left.x - right.x, left.y - right.y);
 	}
-
-	public static Vec2 operator +(Vec2 left, Vec2 right)
+	public static Vec2 operator -(float left, Vec2 right)
 	{
-		return new Vec2(left.x + right.x, left.y + right.y);
+		return new Vec2(left - right.x, left - right.y);
+	}
+	public static Vec2 operator -(Vec2 left, float right)
+	{
+		return new Vec2(left.x - right, left.y - right);
+	}
+
+	public static Vec2 operator *(Vec2 left, Vec2 right) //multiply
+	{
+		return new Vec2(left.x * right.x, left.y * right.y);
+	}
+	public static Vec2 operator *(float left, Vec2 right)
+	{
+		return new Vec2(left * right.x, left * right.y);
+	}
+	public static Vec2 operator *(Vec2 left, float right)
+	{
+		return new Vec2(left.x * right, left.y * right);
 	}
 
 	public override string ToString()
 	{
 		return String.Format("({0},{1})", x, y);
 	}
-}
 
+	public static float DegToRad(float deg)
+	{
+		float radians = deg * (Mathf.PI / 180.0f);
+		return radians;
+	}
+
+	public static float RadToDeg(float rad)
+	{
+		float degrees = rad * (180.0f / Mathf.PI);
+		return degrees;
+	}
+
+	public static Vec2 GetUnitVecRad(float rad)
+	{
+		float newX = Mathf.Cos(rad);
+		float newY = Mathf.Sin(rad);
+		return new Vec2(newX, newY);
+	}
+
+	public static Vec2 GetUnitVecDeg(float deg)
+	{
+		return GetUnitVecRad(DegToRad(deg));
+	}
+
+	public static Vec2 RandomUnitVec()
+	{
+		float random_Deg = Utils.Random(0.0f, 360.0f);
+		return GetUnitVecDeg(random_Deg);
+	}
+
+	public void SetAngleRad(float rad)
+	{
+		float newX = Mathf.Cos(rad) * Length();
+		float newY = Mathf.Sin(rad) * Length();
+		SetXY(newX, newY);
+	}
+
+	public void SetAngleDeg(float deg)
+	{
+		SetAngleRad(DegToRad(deg));
+	}
+
+	public float GetAngleRad()
+	{
+		return Mathf.Atan2(y, x);
+	}
+
+	public float GetAngleDeg()
+	{
+		return RadToDeg(Mathf.Atan2(y, x));
+	}
+
+	public void RotateRad(float rad)
+	{
+		SetXY(x * Mathf.Cos(rad) - y * Mathf.Sin(rad), x * Mathf.Sin(rad) + y * Mathf.Cos(rad));
+	}
+
+	public void RotateDeg(float deg)
+	{
+		RotateRad(DegToRad(deg));
+	}
+
+	public void RotateAroundVecRad(Vec2 RotationPoint, float rad)
+	{
+		this -= RotationPoint;
+		RotateRad(rad);
+		this += RotationPoint;
+	}
+
+	public void RotateAroundVecDeg(Vec2 RotationPoint, float deg)
+	{
+		RotateAroundVecRad(RotationPoint, DegToRad(deg));
+	}
+
+	public static Vec2 GetMousePosition()
+	{
+		return new Vec2(Input.mouseX, Input.mouseY);
+	}
+}
