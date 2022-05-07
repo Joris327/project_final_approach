654f11146afb1da82acc5d5426c90f6228a91342
Jasper Warlich <jasperwarlich1@outlook.com>
2022-05-07 17:13:55 +0200

    Enemy Collision

---
+++ Project/GXPEngine2022BB/GXPEngine/Game Files/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GXPEngine;
+
+namespace GXPEngine
+{
+    public class Enemy : AnimationSprite
+    {
+
+        LevelManager levelManager;
+        public Enemy(float pX, float pY) : base("barry.png", 7, 1)
+        {
+            SetOrigin(width / 2, height / 2);
+            SetXY(pX, pY);
+            SetFrame(7);
+            this.collider.isTrigger = true;
+        }
+
+        void OnCollision(GameObject other)
+        {
+            if (other is Block)
+            {
+                Console.WriteLine("Hit");
+                this.LateDestroy();
+
+            }
+        }
+    }
+}
--- Project/GXPEngine2022BB/GXPEngine/Game Files/Levels/Level1.cs
+++ Project/GXPEngine2022BB/GXPEngine/Game Files/Levels/Level1.cs
@@ -8,6 +8,8 @@ namespace GXPEngine
         public Level1() : base("colors.png", false, false)
         {
             LateAddChild(new Player(200, 600));
+            LateAddChild(new Enemy(600, 600));
+            LateAddChild(new Enemy(400, 200));
         }
     }
 }
